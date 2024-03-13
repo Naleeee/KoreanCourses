2e3bce0aa92396b43480661f2fecd09e0302ae1f
Nale <nthn.lemale@gmail.com>
2024-03-14 01:29:52 +0900

    Add philo notes and computer graphics project

--- ComputerGraphics/SimpleMaths/main.cpp
+++ ComputerGraphics/SimpleMaths/main.cpp
@@ -2,30 +2,29 @@
 #include <iostream>
 
 #define _USE_MATH_DEFINES
-#include <cmath>
 
 int main() {
-    // Exercise 1
-    cyclone::Vector3 p(1, 4, -5);
-    cyclone::Vector3 v(5, 1, 4);
-    std::cout << (p + v).toString() << std::endl;
+  // Exercise 1
+  cyclone::Vector3 p(1, 4, -5);
+  cyclone::Vector3 v(5, 1, 4);
+  std::cout << (p + v).toString() << std::endl;
 
-    // Exercise 2
-    cyclone::Vector3 v1(1, 0, 0);
-    cyclone::Vector3 v2(0, 1, 0);
-    std::cout << (v1 + v2).toString() << std::endl;
+  // Exercise 2
+  cyclone::Vector3 v1(1, 0, 0);
+  cyclone::Vector3 v2(0, 1, 0);
+  std::cout << (v1 + v2).toString() << std::endl;
 
-    // Exercise 3
-    std::cout << (v2 * 5.0f).toString() << std::endl;
+  // Exercise 3
+  std::cout << (v2 * 5.0f).toString() << std::endl;
 
-    // Exercise 4
-    cyclone::Vector3 p1(8, 2, 5);
-    cyclone::Vector3 p2(1, -1, 4);
-    std::cout << (p2 - p1).toString() << std::endl;
+  // Exercise 4
+  cyclone::Vector3 p1(8, 2, 5);
+  cyclone::Vector3 p2(1, -1, 4);
+  std::cout << (p2 - p1).toString() << std::endl;
 
-    // Exercise 5
-    cyclone::Vector3 v3(1, 3, 4);
-    v3.normalise();
-    std::cout << p1.toString() << std::endl;
-    return 0;
+  // Exercise 5
+  cyclone::Vector3 v3(1, 3, 4);
+  v3.normalise();
+  std::cout << p1.toString() << std::endl;
+  return 0;
 }
--- Philosophy/Notes.md
+++ Philosophy/Notes.md
@@ -5,3 +5,14 @@
 - Unwritten rules
 - Confront yourself with questions
 - Ethics help to control and manage science improvements
+
+## Socrates
+He is an ancient Greek philosopher.
+Dialogues -> cave
+
+Most important quote:
+"I know that I don't know"
+
+Western philo focuses on doubt and argumentation: "critical thinking"
+### Methods
+### Ethics
